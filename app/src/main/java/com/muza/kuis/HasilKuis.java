e503c2609a27b1f66dbc8d622e812134e9e6e003
ahmadmuza <zakir3447@gmail.com>
2022-05-10 18:54:58 -0700

    kuisApp

---
+++ app/src/main/java/com/muza/kuis/HasilKuis.java
@@ -0,0 +1,29 @@
+package com.muza.kuis;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.app.Activity;
+import android.content.Intent;
+import android.view.View;
+import android.widget.TextView;
+
+public class HasilKuis extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_hasil_kuis);
+        TextView hasil = (TextView)findViewById(R.id.hasil);
+        TextView nilai = (TextView)findViewById(R.id.nilai);
+
+        hasil.setText("Jawaban Benar :"+MainActivity.benar+"\nJawaban Salah :"+MainActivity.salah);
+        nilai.setText(""+MainActivity.hasil);
+    }
+
+    public void ulangi(View view){
+        finish();
+        Intent a = new Intent(getApplicationContext(),MainActivity.class);
+        startActivity(a);
+    }
+}
